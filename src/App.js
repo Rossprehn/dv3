2b243422e60a22b0c3fcb7c9570a53064b46e71d
Rossprehn <32393462+Rossprehn@users.noreply.github.com>
2018-01-31 10:27:55 -0700

    redo

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import { JobDetails } from './JobDetails'
+import { InputForm } from './InputForm.js'
+import { Preview } from './Preview.js'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch('./listing.json')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          data: response
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <main>
+          <JobDetails jobData={this.state.data} />
+          <InputForm />
+          <preview />
+        </main>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App
